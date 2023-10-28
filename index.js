d4a9ffd61d5fb9b172e91b49ae7cccce13bfe9cc
SuperChump123 <greatdog14@gmail.com>
2023-10-28 17:33:03 -0400

    change names, create map for node locations

--- index.js
+++
@@ -1,8 +0,0 @@
-const test = require('./maps/union');
-let fs = require('fs');
-
-let nodes = test.getNodes("OSU Public Safety Office", "West Plaza Entrance");
-let nodesObj = {'nodes': nodes}
-
-fs.writeFileSync('data/nodes.json', JSON.stringify(nodesObj, null, ' '), 'utf8');
-

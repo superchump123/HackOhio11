c01df7f48b42c988f8c215faab17c28ae0fcc9af
SuperChump123 <greatdog14@gmail.com>
2023-10-28 14:15:03 -0400

    first floor of union mapped (except espress-oh)

--- maps/union.js
+++ maps/union.js
@@ -1,23 +1,47 @@
 const rf = require('room-finder');
 
+const westPlazaEntrance = new rf.Room("West Plaza Entrance", rf.Direction.FRONT);
+const unionMarket = new rf.Room("Union Market", rf.Direction.LEFT);
+const sloopysDiner = new rf.Room("Sloopy's Diner", rf.Direction.RIGHT);
+const woodysTavern = new rf.Room("Woody's Tavern", rf.Direction.LEFT);
+const bankConference = new rf.Room("US Bank Conference Theater", rf.Direction.RIGHT);
+const perfHallRoom = new rf.Room("Performance Hall", rf.Direction.RIGHT);
+const greatHallRoom = new rf.Room("Great Hall Meeting Room", rf.Direction.LEFT);
+const infoDesk = new rf.Room("Information Desk", rf.Direction.RIGHT);
+const multiculturalCenter = new rf.Room("Multicultural Center", rf.Direction.RIGHT);
+const highStreetEntrance = new rf.Room("High Street Entrance", rf.Direction.FRONT);
 
-function createDirections() {
-    const leftMainHallway = [
-        new rf.Room("")
-    ]
-    return new rf.Building();
+const bookStore = new rf.Room("University Bookstore", rf.Direction.RIGHT);
+const usBank = new rf.Room("US Bank", rf.Direction.LEFT);
+const publicSafety = new rf.Room("OSU Public Safety Office", rf.Direction.RIGHT);
+
+function createBuilding() {
+    const greatHallToBookstoreFork = new rf.Fork(rf.Direction.LEFT, "Great Hall to Bookstore Hallway", "Bookstore Hallway");
+    const mainHallway = new rf.Hallway([
+        westPlazaEntrance,
+        sloopysDiner,
+        bankConference,
+        perfHallRoom,
+        infoDesk,
+        multiculturalCenter,
+        greatHallToBookstoreFork,
+        highStreetEntrance
+    ]);
+
+    const bookstoreHallway = new rf.Hallway([
+        new rf.Fork(rf.Direction.FRONT, rf.reverseConnection("Great Hall to Bookstore Hallway"), "Great Hall"),
+        bookStore,
+        usBank,
+        publicSafety
+    ]);
+
+    return new rf.Building([mainHallway, bookstoreHallway]);
 }
 
-function printDirections() {
+function printDirections(start, end) {
+    const floor1 = createBuilding();
     console.log(
-        new rf.Building([
-            new rf.Hallway([
-                new rf.Room("A", rf.Direction.LEFT),
-                new rf.Room("B", rf.Direction.RIGHT),
-                new rf.Turn(rf.Direction.RIGHT),
-                new rf.Room("C", rf.Direction.LEFT),
-            ]),
-        ]).getDirections("A", "C")
+        floor1.getDirections(start, end)
     );
 }
 
